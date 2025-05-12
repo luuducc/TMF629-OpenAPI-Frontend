31b7a9960fb7007681bea493101f03b0f668cc9c
luuduc <luuduc1708@gmail.com>
2025-05-12 15:50:35 +0700

    create FormListGenerator component to merge repetitive logic

--- src/components/CustomerDialog.vue
+++ src/components/CustomerDialog.vue
@@ -1,22 +1,25 @@
 <script setup lang="ts">
-import { reactive, watch, toRaw, watchEffect, ref } from 'vue';
+import { reactive, watch, toRaw, watchEffect } from 'vue';
 import {
   ContactMediumType, PartyType, AccountType, AgreementType, ValueType, RelationshipType, PaymentMethodType
 } from "@/types"
 import type {
   Customer, ContactMedium, AccountRef, AgreementRef, Characteristic, CreditProfile, PaymentMethodRef, RelatedPartyOrPartyRoleRef
 } from "@/types"
-import { Dialog, Button, InputText, Textarea, Divider } from 'primevue';
+import { Dialog, Button, InputText, Textarea } from 'primevue';
 import { Form } from '@primevue/forms';
-import ContactMediumForm from './form/ContactMediumForm.vue';
-import EngagedPartyForm from './form/EngagedPartyForm.vue';
-import AccountForm from './form/AccountForm.vue';
-import AgreementForm from './form/AgreementForm.vue';
-import CharacteristicForm from './form/CharacteristicForm.vue';
-import CreditProfileForm from './form/CreditProfileForm.vue';
-import PartyRoleSpecificationForm from './form/PartyRoleSpecificationForm.vue';
-import PaymentMethodForm from './form/PaymentMethodForm.vue';
-import RelatedPartyOrPartyRoleForm from './form/RelatedPartyOrPartyRoleForm.vue';
+import {
+  ContactMediumForm,
+  EngagedPartyForm,
+  AccountForm,
+  AgreementForm,
+  CharacteristicForm,
+  CreditProfileForm,
+  PartyRoleSpecificationForm,
+  PaymentMethodForm,
+  RelatedPartyOrPartyRoleForm,
+  FormListGenerator
+} from '@/components/form';
 
 // states
 const visible = defineModel<boolean>('visible');
@@ -162,224 +165,68 @@ watchEffect(() => {
       </div>
 
       <!-- Account -->
-      <div class="flex flex-col gap-2">
-        <div class="flex justify-between items-center">
-          <label class="font-medium">Account</label>
-          <Button 
-            @click="addAccount" 
-            label="New" 
-            icon="pi pi-plus"
-            size="small"
-            outlined
-          />
-        </div>
-
-        <div 
-          v-for="(_, index) in accounts" 
-          :key="index" 
-          class="rounded-xl shadow-sm p-4 border border-gray-200 space-y-2">
-          <div class="flex justify-between items-center mb-2">
-            <label class="text-sm font-medium">Account {{ index + 1 }}</label>
-            <Button 
-              @click="accounts.splice(index, 1)" 
-              label="Delete" 
-              icon="pi pi-trash"
-              severity="danger" 
-              size="small" 
-            />
-          </div>
-          <AccountForm v-model="accounts[index]"/>
-        </div>
-      </div>
+      <FormListGenerator
+        main-label="Account"
+        :list="accounts"
+        :on-add="addAccount"
+        :form-component="AccountForm"
+      />
 
       <!-- Agreement -->
-      <div class="flex flex-col gap-2">
-        <div class="flex justify-between items-center">
-          <label class="font-medium">Agreement</label>
-          <Button 
-            @click="addAgreement" 
-            label="New" 
-            icon="pi pi-plus"
-            size="small"
-            outlined
-          />
-        </div>
-
-        <div 
-          v-for="(_, index) in agreements" 
-          :key="index" 
-          class="rounded-xl shadow-sm p-4 border border-gray-200 space-y-2">
-          <div class="flex justify-between items-center mb-2">
-            <label class="text-sm font-medium">Agreement {{ index + 1 }}</label>
-            <Button 
-              @click="agreements.splice(index, 1)" 
-              label="Delete" 
-              icon="pi pi-trash"
-              severity="danger" 
-              size="small" />
-          </div>
-          <AgreementForm v-model="agreements[index]"/>
-        </div>
-      </div>
+      <FormListGenerator
+        main-label="Agreement"
+        :list="agreements"
+        :on-add="addAgreement"
+        :form-component="AccountForm"
+      />
 
       <!-- Characteristic -->
-      <div class="flex flex-col gap-2">
-        <div class="flex justify-between items-center">
-          <label class="font-medium">Characteristic</label>
-          <Button 
-            @click="addCharacteristic" 
-            label="New" 
-            icon="pi pi-plus"
-            size="small"
-            outlined
-          />
-        </div>
-
-        <div 
-          v-for="(_, index) in characteristics" 
-          :key="index" 
-          class="rounded-xl shadow-sm p-4 border border-gray-200 space-y-2">
-          <div class="flex justify-between items-center mb-2">
-            <label class="text-sm font-medium">Characteristic {{ index + 1 }}</label>
-            <Button 
-              @click="characteristics.splice(index, 1)" 
-              label="Delete" 
-              icon="pi pi-trash"
-              severity="danger" 
-              size="small" 
-            />
-          </div>
-          <CharacteristicForm v-model="characteristics[index]"/>
-        </div>
-      </div>
+      <FormListGenerator
+        main-label="Characteristic"
+        :list="characteristics"
+        :on-add="addCharacteristic"
+        :form-component="CharacteristicForm"
+      />
 
       <!-- Contact Medium -->
-      <div class="flex flex-col gap-2">
-        <div class="flex justify-between items-center">
-          <label class="font-medium">Contact Medium</label>
-          <Button 
-            @click="addContactMedium" 
-            label="New" 
-            icon="pi pi-plus"
-            size="small"
-            outlined
-          />
-        </div>
-
-        <div 
-          v-for="(_, index) in contactMediums" 
-          :key="index" 
-          class="rounded-xl shadow-sm p-4 border border-gray-200 space-y-2">
-          <div class="flex justify-between items-center mb-2">
-            <label class="text-sm font-medium">Medium {{ index + 1 }}</label>
-            <Button 
-              @click="contactMediums.splice(index, 1)" 
-              label="Delete" 
-              icon="pi pi-trash"
-              severity="danger" 
-              size="small" 
-            />
-          </div>
-          <ContactMediumForm v-model="contactMediums[index]" />
-        </div>
-      </div>
+      <FormListGenerator
+        main-label="Contact medium"
+        sub-label="Medium"
+        :list="contactMediums"
+        :on-add="addContactMedium"
+        :form-component="ContactMediumForm"
+      />
 
       <!-- Credit Profile -->
-      <div class="flex flex-col gap-2">
-        <div class="flex justify-between items-center">
-          <label class="font-medium">Credit Profile</label>
-          <Button 
-            @click="addCreditProfile" 
-            label="New" 
-            icon="pi pi-plus"
-            size="small"
-            outlined
-          />
-        </div>
-
-        <div 
-          v-for="(_, index) in creditProfiles" 
-          :key="index" 
-          class="rounded-xl shadow-sm p-4 border border-gray-200 space-y-2">
-          <div class="flex justify-between items-center mb-2">
-            <label class="text-sm font-medium">Profile {{ index + 1 }}</label>
-            <Button 
-              @click="creditProfiles.splice(index, 1)" 
-              label="Delete" 
-              icon="pi pi-trash"
-              severity="danger" 
-              size="small" 
-            />
-          </div>
-          <CreditProfileForm v-model="creditProfiles[index]"/>
-        </div>
-      </div>
+      <FormListGenerator
+        main-label="Credit profile"
+        sub-label="Profile"
+        :list="creditProfiles"
+        :on-add="addCreditProfile"
+        :form-component="CreditProfileForm"
+      />
 
       <!-- Payment Method -->
-      <div class="flex flex-col gap-2">
-        <div class="flex justify-between items-center">
-          <label class="font-medium">Payment method</label>
-          <Button 
-            @click="addPaymentMethod" 
-            label="New" 
-            icon="pi pi-plus"
-            size="small"
-            outlined
-          />
-        </div>
-
-        <div 
-          v-for="(_, index) in paymentMethods" 
-          :key="index" 
-          class="rounded-xl shadow-sm p-4 border border-gray-200 space-y-2">
-          <div class="flex justify-between items-center mb-2">
-            <label class="text-sm font-medium">Method {{ index + 1 }}</label>
-            <Button 
-              @click="paymentMethods.splice(index, 1)" 
-              label="Delete" 
-              icon="pi pi-trash"
-              severity="danger" 
-              size="small" 
-            />
-          </div>
-          <PaymentMethodForm v-model="paymentMethods[index]"/>
-        </div>
-      </div>
+      <FormListGenerator
+        main-label="Payment method"
+        sub-label="Method"
+        :list="paymentMethods"
+        :on-add="addPaymentMethod"
+        :form-component="PaymentMethodForm"
+      />
 
       <!-- Related Party Or Party Role -->
-      <div class="flex flex-col gap-2">
-        <div class="flex justify-between items-center">
-          <label class="font-medium">Related party</label>
-          <Button 
-            @click="addRelatedParty" 
-            label="New" 
-            icon="pi pi-plus"
-            size="small"
-            outlined
-          />
-        </div>
-
-        <div 
-          v-for="(_, index) in relatedParties" 
-          :key="index" 
-          class="rounded-xl shadow-sm p-4 border border-gray-200 space-y-2">
-          <div class="flex justify-between items-center mb-2">
-            <label class="text-sm font-medium">Party {{ index + 1 }}</label>
-            <Button 
-              @click="relatedParties.splice(index, 1)" 
-              label="Delete" 
-              icon="pi pi-trash"
-              severity="danger" 
-              size="small" 
-            />
-          </div>
-          <RelatedPartyOrPartyRoleForm v-model="relatedParties[index]"/>
-        </div>
-      </div>
+      <FormListGenerator
+        main-label="Related party"
+        sub-label="Party"
+        :list="relatedParties"
+        :on-add="addRelatedParty"
+        :form-component="RelatedPartyOrPartyRoleForm"
+      />
 
     </Form>
+    
     <template #footer>
-      <!-- Buttons -->
       <div class="flex justify-end gap-3">
         <Button 
           type="submit" 
